741229e0c96f732d01ef524197826cc0cd9cc838
Lakshman <lakshmannadendla15122021@gmail.com>
2025-05-20 12:19:27 +0530

    commit1

---
+++ file1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max_file_size 1024
+
+
+int main()
+{
+    char line[max_file_size];
+    FILE *file;
+    file = fopen("input.txt","r");
+    if(file == NULL)
+    {
+        perror("Error opening the file");
+        return 1;
+    }
+    while(fgets(line,sizeof(line),file))
+    {
+        printf("%s",line);
+    }
+    fclose(file);
+    return 0;
+
+}
